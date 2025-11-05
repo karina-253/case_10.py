a976a7678847bb6d015bbad111cc6deed742e016
karina-253 <k.pinoeva@g.nsu.ru>
2025-11-05 09:05:05 +0700

    Update budget.py

--- budget.py
+++ budget.py
@@ -45,33 +45,3 @@ def compare_budget_vs_actual(budget: dict, transactions: list) -> dict:
             "status": "\u2705" f'{lcl.WITHIN_BUDGET}' if diff >= 0 else "⚠️" f'{lcl.BUDGET_EXCEEDED}'
         }
     return report
-
-
-# ==========================
-# VISUALIZATION
-# ==========================
-
-def visualize_financial_data(transactions: list):
-    """
-    Visualizes expenses by category using a bar chart.
-
-    Args:
-        transactions (list): List of transaction dictionaries.
-    """
-    if not transactions:
-        print(f'{lcl.NO_VISUALIZATION_DATA}')
-        return
-
-    expenses = defaultdict(float)
-    for t in transactions:
-        if t["amount"] < 0:
-            expenses[t["category"]] += abs(t["amount"])
-    if expenses:
-        plt.figure(figsize=(8, 5))
-        plt.bar(expenses.keys(), expenses.values())
-        plt.title(f'{lcl.EXPENSES_BY_CATEGORY}')
-        plt.xlabel(f'{lcl.CATEGORY}')
-        plt.ylabel(f'{lcl.AMOUNT_RUB}')
-        plt.xticks(rotation=45, ha="right")
-        plt.tight_layout()
-        plt.show()
